12e697b6f323517e23a878fbf517c9048489d27c
rania1237 <rania.regei@esprit.tn>
2026-03-02 22:12:06 +0100

    Add microservices structure and updates

--- backEnd/microservices/Formation/src/main/java/skillup/demo/FormationRepository.java
+++
@@ -1,7 +0,0 @@
-package skillup.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormationRepository extends JpaRepository<Formation, Long> {}
---
+++ backEnd/microservices/user/src/main/java/tn/esprit/user/Role.java
@@ -0,0 +1,4 @@
+package tn.esprit.user;
+
+public class Role {
+}
