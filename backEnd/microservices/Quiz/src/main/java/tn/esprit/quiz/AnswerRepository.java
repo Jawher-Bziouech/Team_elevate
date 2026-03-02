2210f7b6e62c8d479679382b8abc1b8cd13ba228
wassim hammouda <100074389+Therealcydex@users.noreply.github.com>
2026-03-02 21:23:06 +0100

    Revert "Quiz"

--- backEnd/microservices/Quiz/src/main/java/tn/esprit/quiz/AnswerRepository.java
+++ backEnd/microservices/Quiz/src/main/java/tn/esprit/quiz/AnswerRepository.java
@@ -1,8 +1,4 @@
 package tn.esprit.quiz;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
+public interface AnswerRepository {
 }
--- backEnd/microservices/Quiz/src/main/java/tn/esprit/quiz/QuizRepository.java
+++ backEnd/microservices/Quiz/src/main/java/tn/esprit/quiz/QuizRepository.java
@@ -1,8 +1,4 @@
 package tn.esprit.quiz;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
+public interface QuizRepository {
 }
